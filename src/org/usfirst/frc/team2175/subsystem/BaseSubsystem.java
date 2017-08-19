8bd04643178afe7fe3ee5d083df55092e09587fd
Forthwind <kl.forthwind@gmail.com>
2017-08-18 19:35:14 -0500

    Add the correct subsystems

---
+++ src/org/usfirst/frc/team2175/subsystem/BaseSubsystem.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc.team2175.subsystem;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class BaseSubsystem extends Subsystem {
+
+	@Override
+	protected void initDefaultCommand() {
+	}
+
+}
---
+++ src/org/usfirst/frc/team2175/subsystem/VisionSubsystem.java
@@ -0,0 +1,5 @@
+package org.usfirst.frc.team2175.subsystem;
+
+public class VisionSubsystem extends BaseSubsystem {
+
+}
