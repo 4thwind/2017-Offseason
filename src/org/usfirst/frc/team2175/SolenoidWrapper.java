ebce84bd7e21cffa01cf4d538408a6af767f398c
Kevin La Casse <kl.forthwind@gmail.com>
2017-09-11 12:24:43 -0500

    Deal with Solenoid Wrapper giving errors Commented it out for now

--- src/org/usfirst/frc/team2175/SolenoidWrapper.java
+++ src/org/usfirst/frc/team2175/SolenoidWrapper.java
@@ -1,48 +1,48 @@
-package org.usfirst.frc.team2175;
-
-import org.usfirst.frc.team2175.Info.SolenoidInfo;
-
-import edu.wpi.first.wpilibj.DoubleSolenoid;
-import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
-import edu.wpi.first.wpilibj.Solenoid;
-
-public class SolenoidWrapper {
-	private DoubleSolenoid doubleSolenoid;
-	private Solenoid solenoid;
-	private boolean isDouble;
-
-	public SolenoidWrapper(final SolenoidInfo solenoidInfo) {
-		if (solenoidInfo.type.equals("single")) {
-			solenoid = new Solenoid(solenoidInfo.ports[0]);
-			isDouble = false;
-		} else {
-			doubleSolenoid = new DoubleSolenoid(solenoidInfo.ports[0],
-					solenoidInfo.ports[1]);
-			isDouble = true;
-		}
-	}
-
-	public void set(final boolean on) {
-		if (isDouble) {
-			if (on) {
-				doubleSolenoid.set(Value.kForward);
-			} else {
-				doubleSolenoid.set(Value.kReverse);
-			}
-		} else {
-			solenoid.set(on);
-		}
-	}
-
-	public boolean get() {
-		if (isDouble) {
-			if (doubleSolenoid.get() == Value.kForward) {
-				return true;
-			} else {
-				return false;
-			}
-		} else {
-			return solenoid.get();
-		}
-	}
-}
+//package org.usfirst.frc.team2175;
+//
+//import org.usfirst.frc.team2175.Info.SolenoidInfo;
+//
+//import edu.wpi.first.wpilibj.DoubleSolenoid;
+//import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
+//import edu.wpi.first.wpilibj.Solenoid;
+//
+//public class SolenoidWrapper {
+//	private DoubleSolenoid doubleSolenoid;
+//	private Solenoid solenoid;
+//	private boolean isDouble;
+//
+//	public SolenoidWrapper(final SolenoidInfo solenoidInfo) {
+//		if (solenoidInfo.type.equals("single")) {
+//			solenoid = new Solenoid(solenoidInfo.ports[0]);
+//			isDouble = false;
+//		} else {
+//			doubleSolenoid = new DoubleSolenoid(solenoidInfo.ports[0],
+//					solenoidInfo.ports[1]);
+//			isDouble = true;
+//		}
+//	}
+//
+//	public void set(final boolean on) {
+//		if (isDouble) {
+//			if (on) {
+//				doubleSolenoid.set(Value.kForward);
+//			} else {
+//				doubleSolenoid.set(Value.kReverse);
+//			}
+//		} else {
+//			solenoid.set(on);
+//		}
+//	}
+//
+//	public boolean get() {
+//		if (isDouble) {
+//			if (doubleSolenoid.get() == Value.kForward) {
+//				return true;
+//			} else {
+//				return false;
+//			}
+//		} else {
+//			return solenoid.get();
+//		}
+//	}
+//}
