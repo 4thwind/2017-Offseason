2fe77a1a0bc3b0691be53241d99e92e6f9dc5625
Kevin La Casse <kl.forthwind@gmail.com>
2017-09-11 21:02:40 -0500

    Finish up branch Get rid of some prefs

--- src/org/usfirst/frc/team2175/info/InfoLocator.java
+++ src/org/usfirst/frc/team2175/info/InfoLocator.java
@@ -6,19 +6,19 @@ import org.usfirst.frc.team2175.ServiceLocator;
 
 public class InfoLocator {
 	private HashMap<String, String> wiringInfo;
-	private final boolean isComp = true;
+	private boolean isComp = true;
 
 	public InfoLocator() {
 		makeWiringInfoMap();
 		ServiceLocator.register(this);
 	}
 
-	private void makeWiringInfoMap() {
-		wiringInfo = new HashMap<>();
-		WiringInfo.fillWiringMap(isComp, wiringInfo);
-	}
-
 	public String getWiringInfo(String id) {
 		return wiringInfo.get(id);
+	}
+
+	private void makeWiringInfoMap() {
+		WiringProps wiringProps = new WiringProps();
+		wiringInfo = wiringProps.getWiringMap(isComp);
 	}
 }
